2c2a9f01f419d6cb715e7fd7bbd38625e03f9aeb
fathimanasrin063 <fathimanasrin063@gmail.com>
2026-02-19 11:33:35 +0530

    Add files via upload

---
+++ stringmanipulation.java
@@ -0,0 +1,10 @@
+import java.io.*;
+import java.util.*;
+class string manipulation 
+{
+public static void main(String []args)
+{
+Scanner s=new Scanner(System.in);
+String a,b,c
+System.out.printl("enetr a string:");
+ans
