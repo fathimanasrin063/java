ce7c01d9dd103f8994703d9d535d558bbf969f6e
fathimanasrin063 <fathimanasrin063@gmail.com>
2026-03-11 12:23:56 +0530

    Add files via upload

---
+++ average.java
@@ -0,0 +1,42 @@
+import java.io.*;
+import java.util.*;
+class negativeNumberException extends Exception
+{
+public negativeNumberException (String s)
+{
+super(s);
+}
+}
+class average
+{
+public static void main(String args[])
+{
+Scanner sc=new Scanner(System.in);
+int count,i;
+System.out.println("Please Enter Numbers to be calculated:");
+count=sc.nextInt();
+int[]numbers=new int[count];
+try
+{
+System.out.println("Please Enter"+count+"numbers:");
+for(i=0;i<count;i++)
+{
+int num=sc.nextInt();
+numbers[i]=num;
+if(num<0)
+{
+throw new negativeNumberException("Negative Numbers are not allowed");
+}
+}
+int sum=0;
+for(i=0;i<count;i++)
+{sum+=numbers[i];
+}
+float avg=(float)sum/count;
+System.out.println("Average of given numbers is:"+avg);
+}
+catch(negativeNumberException ex)
+{
+System.out.println("Exception caught!!");
+System.out.println(ex.getMessage());
+}}}
